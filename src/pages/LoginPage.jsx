69aa3f9d0c90aabf00fe67908522ccc38c63831f
Leanne Huang <leanne110x@gmail.com>
2025-11-02 18:59:38 +0800

    update login and signup form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,30 @@
 import LoginForm from "../components/LoginForm";
 
 function LoginPage() {
-  return <LoginForm />;
+
+  const backgroundImage =
+    "https://static.vecteezy.com/system/resources/previews/066/583/774/non_2x/dog-background-adorable-group-of-dogs-and-cats-posing-together-for-a-studio-pet-portrait-free-photo.jpg";
+
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "20px",
+      }}
+    >
+
+      <LoginForm />
+
+    </div>
+  );
+
 }
 
 export default LoginPage;
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,7 +1,27 @@
 import SignupForm from "../components/SignupForm";
 
 function SignupPage() {
-  return <SignupForm />;
+
+  const backgroundImage =
+    "https://static.vecteezy.com/system/resources/previews/066/583/774/non_2x/dog-background-adorable-group-of-dogs-and-cats-posing-together-for-a-studio-pet-portrait-free-photo.jpg";
+
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "20px",
+      }}
+    >
+      <SignupForm />
+    </div>
+  );;
 }
 
 export default SignupPage;
